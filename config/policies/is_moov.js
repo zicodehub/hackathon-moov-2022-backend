2ea960d723b418b0285e3eeda8d8f8ceacb1b1f3
marcleord <marcleord@gmail.com>
2022-06-17 23:58:54 +0000

    test services

--- config/policies/is_moov.js
+++ config/policies/is_moov.js
@@ -1,7 +1,7 @@
 module.exports = async (ctx, next) => {
     console.log(strapi.config, Object.keys(strapi.config))
-    if(!strapi.config.services.number.is_moov("12921892")) {
+    // if(!strapi.config.services.number.is_moov("12921892")) {
         return await next()
-    }
+    // }
     ctx.unauthaured("Bonjour inconnu")
 }

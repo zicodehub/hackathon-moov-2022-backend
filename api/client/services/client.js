126cf2400f2fc891205ea459450ad607586ff14c
marcleord <marcleord@gmail.com>
2022-06-18 00:15:55 +0000

    test services

--- api/client/services/client.js
+++ api/client/services/client.js
@@ -4,12 +4,12 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
-
+imp
 module.exports = {
     is_ten_digits: (num) => num.length == 10,
 
     is_moov: (num) => {
-        return this.is_ten_digits(num) && num.startsWith("01")
+        return num.startsWith("01")
     }
 
 };

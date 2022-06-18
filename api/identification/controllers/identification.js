0d068807df8952b856cfa4181d7ab7cc430bbe81
marcleord <marcleord@gmail.com>
2022-06-18 00:35:37 +0000

    test service

--- api/identification/controllers/identification.js
+++ api/identification/controllers/identification.js
@@ -1,15 +1,24 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
-    create: async (params) => {
-        console.log(params)
-        return {
-            "ok": "yes"
-        }
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.create(data, { files });
+    } else {
+      entity = await strapi.services.restaurant.create(ctx.request.body);
     }
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
 };
+
+ 
+
+ 

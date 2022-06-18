d6b19cc5128a19b68d607818e86cdb02e2cbdef6
marcleord <marcleord@gmail.com>
2022-06-18 02:53:12 +0000

    identification

--- api/identification/controllers/identification.js
+++ api/identification/controllers/identification.js
@@ -13,23 +13,32 @@ module.exports = {
     console.log("C'est moi")
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
-      const { client, prenom, lieu_naissance, civilite, adresse, profession, date_naissance, nationalite } = data
+      const { nom, prenom, lieu_naissance, civilite, adresse, profession, date_naissance, nationalite } = data
       let created_client = await strapi.services.client.create({
-        client, prenom, lieu_naissance, civilite, adresse, profession, date_naissance, nationalite, 
-      }) 
-      
-      entity = await strapi.services.identification.create(data, { files });
+          client, prenom, lieu_naissance, civilite, adresse, profession, date_naissance, nationalite, 
+        }) 
+        
+        entity = await strapi.services.identification.create(data, { files });
     } else {
         const { numero, msisdn } = ctx.request.body
         const { type_piece, numero_piece, lieu_delivrance, date_emission, date_expiration } = ctx.request.body
-        // let created_sim = await strapi.service['carte-sim']
+        const { nom, prenom, lieu_naissance, civilite, adresse, profession, date_naissance, nationalite } = data
+        
         console.log("La carte sim ", Object.keys(strapi.services))
-        let created_client = await strapi.services.client.create({
-            nom, prenom, lieu_naissance, civilite, adresse, profession, nationalite, 
-        }) 
         let created_piece = await  strapi.services.client.create({ 
             type_piece, numero_piece, lieu_delivrance, date_emission, date_expiration 
         })
+
+        
+        let created_client = await strapi.services.client.create({
+            nom, prenom, lieu_naissance, date_naissance, civilite, adresse, profession, nationalite, 
+            piece_identite: created_client.id
+        }) 
+        
+        let created_sim = await strapi.serviceS['carte-sim'].create({
+            numero, msisdn, client: created_client.id
+        })
+       
       entity = await strapi.services.identification.create(ctx.request.body);
     }
 
